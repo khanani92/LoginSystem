4d4450ee9d07a89d9df01b40d791417c9f711c9a
khanani92 <muddassir_92@hotmail.com>
2013-04-14 10:57:48 +0500

    bootstrape done working on show user

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,28 @@
 /*
  * GET users listing.
  */
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/Sirsyed');
+//var q = require('q');
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+    console.log("db connected")
+});
+
+
 
 exports.list = function(req, res){
-  res.send("respond with a resource");
+    student.find({},function(err,data){
+        if(err){
+            console.log("error"+err);
+        }else{
+            console.log(data);
+        }
+    });
+
+    res.send("done");
+
 };
